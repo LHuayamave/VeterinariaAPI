38ba94030a3576d041eb6f033f9383a066e32c4f
LHuayamave <hbweyb@gmail.com>
2023-06-26 15:25:40 -0500

    Agregar archivos de proyecto.

---
+++ VeterinariaAPI/Entidades/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VeterinariaAPI.Entidades
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string Apellido { get; set; }
+        [Required]
+        public DateTime FechaNac { get; set; }
+        [Required]
+        public string Telefono { get; set; }
+        [Required]
+        public string Direccion { get; set; }
+
+     }
+}
---
+++ VeterinariaAPI/Entidades/Paciente.cs
@@ -0,0 +1,14 @@
+﻿namespace VeterinariaAPI.Entidades
+{
+    public class Paciente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public DateTime FechaNac { get; set; }
+        public string Color { get; set; }
+        public string Senias { get; set; }
+        public string Foto { get; set; }
+        public int ClienteId { get; set; }
+        public Cliente Cliente { get; set; }
+    }
+}

b21cc4beafcb0c0f2e3bd57b72c4c178d9d7aba2
LHuayamave <hbweyb@gmail.com>
2023-06-28 11:10:39 -0500

    Cambios en la configuracion del proyecto

--- VeterinariaAPI/Program.cs
+++ VeterinariaAPI/Program.cs
@@ -1,17 +1,14 @@
+using VeterinariaAPI;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var startup = new Startup(builder.Configuration);
 
-builder.Services.AddControllers();
+// Add services to the container.
+startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+startup.Configure(app, app.Environment);
 
 app.Run();

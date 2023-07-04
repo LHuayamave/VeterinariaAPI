da8bd122faef25516d944d8fe1e65d175fc601a9
Kenny-ec <kepizarro@est.ecotec.edu.ec>
2023-07-03 20:44:59 -0500

    creando entidades y descomentando todo para configurar endpoints

--- VeterinariaAPI/Controllers/TieFacturasDetallesController.cs
+++ VeterinariaAPI/Controllers/TieFacturasDetallesController.cs
@@ -1,46 +1,46 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using VeterinariaAPI.Conexiones;
-using VeterinariaAPI.Entidades;
+//using VeterinariaAPI.Conexiones;
+//using VeterinariaAPI.Entidades;
 
 namespace VeterinariaAPI.Controllers
 {
-    [Route("api/facturasDetalles")]
-    [ApiController]
-    public class TieFacturasDetallesController : ControllerBase
-    {
-        private readonly veterinariaContext _context;
-        public TieFacturasDetallesController(veterinariaContext context)
-        {
-            _context = context;
-        }
+    //[Route("api/facturasDetalles")]
+    //[ApiController]
+    //public class TieFacturasDetallesController : ControllerBase
+    //{
+    //    private readonly veterinariaContext _context;
+    //    public TieFacturasDetallesController(veterinariaContext context)
+    //    {
+    //        _context = context;
+    //    }
 
-        [HttpGet]
-        [Route("listado")]
-        public async Task<ActionResult<List<TieFacturaDetalle>>> Get()
-        {
-            try
-            {
-                return await _context.TieFacturaDetalles.ToListAsync();
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(StatusCodes.Status200OK, new { message = ex.Message });
-            }
-        }
-        [HttpGet]
-        [Route("obtener/{id:int}")]
-        public async Task<ActionResult<TieFacturaDetalle>> Get(int id)
-        {
-            var facturaDetalle = await _context.TieFacturaDetalles.FirstOrDefaultAsync(x => x.IdFacturaDetalle == id);
+    //    [HttpGet]
+    //    [Route("listado")]
+    //    public async Task<ActionResult<List<TieFacturaDetalle>>> Get()
+    //    {
+    //        try
+    //        {
+    //            return await _context.TieFacturaDetalles.ToListAsync();
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            return StatusCode(StatusCodes.Status200OK, new { message = ex.Message });
+    //        }
+    //    }
+    //    [HttpGet]
+    //    [Route("obtener/{id:int}")]
+    //    public async Task<ActionResult<TieFacturaDetalle>> Get(int id)
+    //    {
+    //        var facturaDetalle = await _context.TieFacturaDetalles.FirstOrDefaultAsync(x => x.IdFacturaDetalle == id);
 
-            if (facturaDetalle == null)
-            {
-                return NotFound();
-            }
-            return facturaDetalle;
+    //        if (facturaDetalle == null)
+    //        {
+    //            return NotFound();
+    //        }
+    //        return facturaDetalle;
 
-        }
-    }
+    //    }
+    //}
 }
---
+++ VeterinariaAPI/Entidades/TieFormaPago.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VeterinariaAPI.Entidades;
+
+public partial class TieFormaPago
+{
+    public int IdFormaPago { get; set; }
+
+    public string Nombre { get; set; }
+
+    public virtual ICollection<TieFacturaFormaPago> TieFacturaFormaPagos { get; set; } = new List<TieFacturaFormaPago>();
+}

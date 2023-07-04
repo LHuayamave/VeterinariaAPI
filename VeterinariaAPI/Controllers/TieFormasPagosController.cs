7651feeb7f0324b36a952e980a1e7ca20ee8e79d
Kenny-ec <kepizarro@est.ecotec.edu.ec>
2023-07-04 11:11:51 -0500

    Agregando Api a TieFormaPagoController

--- VeterinariaAPI/Controllers/TieFormasPagosController.cs
+++ VeterinariaAPI/Controllers/TieFormasPagosController.cs
@@ -29,5 +29,85 @@ namespace VeterinariaAPI.Controllers
                 return StatusCode(StatusCodes.Status200OK, new { message = ex.Message });
             }
         }
+
+        [HttpGet]
+        [Route("obtener/{id:int}")]
+        public async Task<ActionResult<TieFormaPago>> Get(int id)
+        {
+            try
+            {
+                var formaPago = await _context.TieFormaPagos.FirstOrDefaultAsync(x => x.IdFormaPago == id);
+
+                if (formaPago == null)
+                {
+                    return NotFound();
+                }
+                return formaPago;
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status200OK, new { message = ex.Message });
+            }
+
+        }
+
+        [HttpPost]
+        [Route("insertar")]
+        public async Task<ActionResult> Post(TieFormaPago tieFormaPago)
+        {
+            try
+            {
+                _context.Add(tieFormaPago);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status200OK, new { message = ex.Message });
+            }
+        }
+
+        [HttpPut("actualizar/{id:int}")] // api/actualizar/#
+        public async Task<ActionResult> Put(int id, TieFormaPago tieFormaPago)
+        {
+            try
+            {
+                var existeFormaPago = await _context.TieFormaPagos.AnyAsync(x => x.IdFormaPago == id);
+                if (!existeFormaPago)
+                {
+                    return NotFound();
+                }
+                _context.Update(tieFormaPago);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status200OK, new { message = ex.Message });
+            }
+        }
+
+        [HttpDelete("eliminar/{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+                var existeFormaPago = await _context.TieFormaPagos.AnyAsync(x => x.IdFormaPago == id);
+                if (!existeFormaPago)
+                {
+                    return NotFound();
+                }
+                _context.Remove(new TieFormaPago() { IdFormaPago = id });
+                await _context.SaveChangesAsync();
+
+                return Ok();
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status200OK, new { message = ex.Message });
+            }
+
+        }
     }
 }

37c1cf3550282c0fd74c6a8c7a25a16d77b6e2c1
Kenny-ec <kepizarro@est.ecotec.edu.ec>
2023-07-04 10:37:07 -0500

    Configuro Api a TieEmpresaController

--- VeterinariaAPI/Controllers/EmpresasController.cs
+++ VeterinariaAPI/Controllers/EmpresasController.cs
@@ -29,5 +29,87 @@ namespace VeterinariaAPI.Controllers
                 return StatusCode(StatusCodes.Status200OK, new { message = ex.Message });
             }
         }
+
+        [HttpGet]
+        [Route("obtener/{id:int}")]
+        public async Task<ActionResult<TieEmpresa>> Get(int id)
+        {
+            try
+            {
+                var empresa = await _context.TieEmpresas.FirstOrDefaultAsync(x => x.IdEmpresa == id);
+
+                if (empresa == null)
+                {
+                    return NotFound();
+                }
+                return empresa;
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status200OK, new { message = ex.Message });
+            }
+        }
+
+        [HttpPost]
+        [Route("insertar")]
+        public async Task<ActionResult> Post(TieEmpresa tieEmpresa)
+        {
+            try
+            {
+                _context.Add(tieEmpresa);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status200OK, new { message = ex.Message });
+            }
+
+        }
+
+        [HttpPut("actualizar/{id:int}")] // api/autores/#
+        public async Task<ActionResult> Put(TieEmpresa tieEmpresa, int id)
+        {
+            try
+            {
+                var existeEmpresa = await _context.TieEmpresas.AnyAsync(x => x.IdEmpresa == id);
+                if (!existeEmpresa)
+                {
+                    return NotFound();
+                }
+
+                _context.Update(tieEmpresa);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status200OK, new { message = ex.Message });
+            }
+
+        }
+
+        [HttpDelete("eliminar/{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+                var existeEmpresa = await _context.TieEmpresas.AnyAsync(x => x.IdEmpresa == id);
+                if (!existeEmpresa)
+                {
+                    return NotFound();
+                }
+                _context.Remove(new TieEmpresa() { IdEmpresa = id });
+                await _context.SaveChangesAsync();
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status200OK, new { message = ex.Message });
+            }
+
+        }
     }
 }

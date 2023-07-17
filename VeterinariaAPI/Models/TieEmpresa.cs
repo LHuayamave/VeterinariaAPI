a4451508e22c88aaecd164f44544ddd7ab0f8970
Kenny Pizarro <kepizarro@est.ecotec.edu.ec>
2023-07-17 01:04:49 -0500

    Agrego validaciones a la clase Empresa

--- VeterinariaAPI/Models/TieEmpresa.cs
+++ VeterinariaAPI/Models/TieEmpresa.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
+using VeterinariaAPI.Negocio;
 
 namespace VeterinariaAPI.Entidades;
 
@@ -11,12 +12,16 @@ public partial class TieEmpresa
     [Required(ErrorMessage = "El campo {0} es requerido")]
     public string NombreEmpresa { get; set; }
     [Required(ErrorMessage = "El campo {0} es requerido")]
+    [SoloNumeros]
+    [StringLength(13,MinimumLength = 10, ErrorMessage = "El campo {0} debe tener entre {2} y {1} caracteres")]
     public string NumDocumento { get; set; }
     [Required(ErrorMessage = "El campo {0} es requerido")]
     public string DireccionEmpresa { get; set; }
     [Required(ErrorMessage = "El campo {0} es requerido")]
+    [EmailAddress(ErrorMessage = "El campo {0} debe ser una dirección de correo electrónico válida.")]
     public string CorreoEmpresa { get; set; }
     [Required(ErrorMessage = "El campo {0} es requerido")]
+    [SoloNumeros]
     public string TelefonoEmpresa { get; set; }
     [JsonIgnore]
     public virtual ICollection<TieFacturaCabecera> TieFacturaCabeceras { get; set; } = new List<TieFacturaCabecera>();

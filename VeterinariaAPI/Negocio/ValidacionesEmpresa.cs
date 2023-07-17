375b5d8b38ad36ac22ea6fc2ab4a759ec71c01d9
Kenny Pizarro <kepizarro@est.ecotec.edu.ec>
2023-07-17 11:21:54 -0500

    Agrego metodo para validar si existe el id empresa

--- VeterinariaAPI/Negocio/ValidacionesEmpresa.cs
+++ VeterinariaAPI/Negocio/ValidacionesEmpresa.cs
@@ -13,6 +13,10 @@ namespace VeterinariaAPI.Negocio
             _context = context;
         }
 
+        public async Task<bool> validarIdEmpresa(TieEmpresa tieEmpresa)
+        {
+            return await _context.TieEmpresas.AnyAsync(x => x.IdEmpresa == tieEmpresa.IdEmpresa);
+        }
         public async Task<bool> validarNumeroDocumento(TieEmpresa tieEmpresa)
         {
             return await _context.TieEmpresas.AnyAsync(x => x.NumDocumento == tieEmpresa.NumDocumento);

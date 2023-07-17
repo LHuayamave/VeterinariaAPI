d037370f06b0160d9f78ece2cdf29274136a8966
Kenny Pizarro <kepizarro@est.ecotec.edu.ec>
2023-07-17 01:13:43 -0500

    Agrego la validacion Solo Numeros

---
+++ VeterinariaAPI/Negocio/SoloNumerosAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace VeterinariaAPI.Negocio
+{
+    public class SoloNumerosAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null || string.IsNullOrEmpty((string)value))
+            {
+                return ValidationResult.Success;
+            }
+
+            if (!Regex.IsMatch((string)value, @"^[0-9]+$"))
+            {
+                return new ValidationResult("Solo se permiten números");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}

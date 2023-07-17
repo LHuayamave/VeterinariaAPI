75836a84bc9d97272f9ad65934dbe63ef7f7bc7f
Kenny Pizarro <kepizarro@est.ecotec.edu.ec>
2023-07-17 11:22:45 -0500

    Agrego metodo de validacion solo letras

---
+++ VeterinariaAPI/Negocio/SoloLetrasAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace VeterinariaAPI.Negocio
+{
+    public class SoloLetrasAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null || string.IsNullOrEmpty((string)value))
+            {
+                return ValidationResult.Success;
+            }
+
+            if (!Regex.IsMatch((string)value, @"^[a-zA-Z]+$"))
+            {
+                return new ValidationResult("Solo se permiten letras");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}

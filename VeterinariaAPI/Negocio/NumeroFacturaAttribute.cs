1a10985047eb00dd2afa1f6d4bb3cc5131d92b8f
Kenny Pizarro <kepizarro@est.ecotec.edu.ec>
2023-07-18 10:20:08 -0500

    Agrego Validacion Numero Factura, admite numeros y guiones

---
+++ VeterinariaAPI/Negocio/NumeroFacturaAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace VeterinariaAPI.Negocio
+{
+    public class NumeroFacturaAttribute: ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null || string.IsNullOrEmpty((string)value))
+            {
+                return ValidationResult.Success;
+            }
+
+            if (!Regex.IsMatch((string)value, @"^[0-9\-]+$"))
+            {
+                return new ValidationResult("Solo se permiten números y guiones");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}

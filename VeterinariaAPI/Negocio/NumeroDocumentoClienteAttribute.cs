72f715b72f403d867b17feb3721a98e571f0cf7d
Kenny Pizarro <kepizarro@est.ecotec.edu.ec>
2023-07-18 10:19:37 -0500

    Agrego Validacion Numero Documento Cliente

---
+++ VeterinariaAPI/Negocio/NumeroDocumentoClienteAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace VeterinariaAPI.Negocio
+{
+    public class NumeroDocumentoClienteAttribute: ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null || string.IsNullOrEmpty((string)value))
+            {
+                return ValidationResult.Success;
+            }
+
+            if (!Regex.IsMatch((string)value, @"^[0-9A-Z]+$"))
+            {
+                return new ValidationResult("Solo se permiten números y letras mayúsculas");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
